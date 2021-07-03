f3b8ec301081d6ac8b91d428889a2f917fb27ade
Dean Djermanovic <dean.djermanovic@five.agency>
2021-07-03 21:48:43 +0200

    Add docs

--- app/src/main/java/com/aptoglobal/playground/core/connectivity/NetworkConnectivityMonitor.kt
+++ app/src/main/java/com/aptoglobal/playground/core/connectivity/NetworkConnectivityMonitor.kt
@@ -4,5 +4,8 @@ import kotlinx.coroutines.flow.Flow
 
 interface NetworkConnectivityMonitor {
 
+    /**
+     * It emits `true` if the device has a valid network connection and is able to reach the internet. Otherwise, it emits `false`.
+     */
     fun onConnectivityChange(): Flow<Boolean>
 }
